b9a3140ac232e43315b573f1a2e199baec7f9a89
Alfintyz <135729486+Alfintyz@users.noreply.github.com>
2023-06-06 23:48:23 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/tokoonline/User.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.tokoonline;
+
+
+public interface User {
+    
+
+    public void setAlamat (String alamat);
+    public void setTelepon (String telepon);
+    
+    public String getNama (int id);
+    public String getAlamat (int id);
+    public String getTelepon (int id);
+}
+
+
---
+++ src/main/java/com/mycompany/tokoonline/main.java
@@ -0,0 +1,23 @@
+
+
+package com.mycompany.tokoonline;
+
+
+public class main {
+
+    public static void main(String[] args) {
+      
+        Member member = new Member ();
+        Barang barang = new Barang ();
+        Transaksi transaksi = new Transaksi ();
+        Laporan laporan = new Laporan ();
+        
+        laporan.laporan (barang);
+        transaksi.prosesTransaksi (member, transaksi, barang);
+        laporan.laporan (barang);
+        laporan.laporan (member);
+        laporan.laporan (transaksi, barang);
+    }
+}   
+    
+
